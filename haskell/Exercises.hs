12e373044c48e357a699f5ca205e60bb11ec43ec
Adiro777 <112520123+Adiro777@users.noreply.github.com>
2024-10-31 17:58:10 -0700

    Finished stuff

--- haskell/Exercises.hs
+++ haskell/Exercises.hs
@@ -1,12 +1,23 @@
 module Exercises
     ( change,
-      -- put the proper exports here
+      firstThenApply,
+      powers,
+      meaningfulLineCount,
+      Shape(Sphere, Box),
+      volume,
+      surfaceArea,
+      BST(Empty),
+      size,
+      contains,
+      insert,
+      inorder
     ) where
 
 import qualified Data.Map as Map
 import Data.Text (pack, unpack, replace)
 import Data.List(isPrefixOf, find)
 import Data.Char(isSpace)
+import Data.Maybe (listToMaybe)
 
 change :: Integer -> Either String (Map.Map Integer Integer)
 change amount
@@ -21,11 +32,75 @@ change amount
                 newCounts = Map.insert d count counts
 
 -- Write your first then apply function here
+firstThenApply :: [a] -> (a -> Bool) -> (a -> b) -> Maybe b
+firstThenApply xs pred function = fmap function (find pred xs)
 
 -- Write your infinite powers generator here
+powers :: Integral a => a -> [a] -- NO GOOD, use integral typeclass instead
+powers base = map (base^) [0..]
 
 -- Write your line count function here
+meaningfulLineCount :: FilePath -> IO Int
+meaningfulLineCount path = do
+  contents <- readFile path
+  return $ length $ filter meaningfulLine $ lines contents
+  where
+    meaningfulLine line = 
+      let trimmed = dropWhile isSpace line
+      in not (null trimmed) && not ("#" `isPrefixOf` trimmed)
 
 -- Write your shape data type here
+data Shape
+  = Sphere Double
+  | Box Double Double Double
+  deriving (Eq, Show)
+
+volume :: Shape -> Double
+volume (Sphere r) = (4 * pi * r^3) / 3
+volume (Box l w h) = l * w * h
+
+surfaceArea :: Shape -> Double
+surfaceArea (Sphere r) = 4 * pi * r^2
+surfaceArea (Box l w h) = 2 * (l * w + l * h + w * h)
 
 -- Write your binary search tree algebraic type here
+data BST a
+    = Empty
+    | Node a (BST a) (BST a)
+
+-- Calculates the size of the tree
+size :: BST a -> Int
+size Empty = 0
+size (Node _ left right) = 1 + size left + size right
+
+-- Inorder traversal of the tree
+inorder :: BST a -> [a]
+inorder Empty = []
+inorder (Node value left right) = inorder left ++ [value] ++ inorder right
+
+-- Inserts a value into the binary search tree
+insert :: Ord a => a -> BST a -> BST a
+insert value Empty = Node value Empty Empty
+insert value (Node nodeValue left right)
+   | value < nodeValue = Node nodeValue (insert value left) right
+   | value > nodeValue = Node nodeValue left (insert value right)
+   | otherwise = Node nodeValue left right
+
+contains :: Ord a => a -> BST a -> Bool
+contains _ Empty = False
+contains value (Node nodeValue left right)
+   | value == nodeValue = True
+   | value < nodeValue = contains value left
+   | otherwise = contains value right
+
+-- Custom Show instance for BST
+-- Custom Show instance for BST
+instance (Show a) => Show (BST a) where
+  show :: Show a => BST a -> String
+  show Empty = "()"
+  show (Node value Empty Empty) = show value
+  show (Node value left Empty) = show left ++ " " ++ show value
+  show (Node value Empty right) = show value ++ " " ++ show right
+  show (Node value left right) = show left ++ " " ++ show value ++ " " ++ show right
+
+
--- typescript/exercises.ts
+++ typescript/exercises.ts
@@ -1,3 +1,5 @@
+import { promises } from "node:dns"
+import { PathLike } from "node:fs"
 import { open } from "node:fs/promises"
 
 export function change(amount: bigint): Map<bigint, bigint> {
@@ -13,12 +15,149 @@ export function change(amount: bigint): Map<bigint, bigint> {
   return counts
 }
 
-// Write your first then apply function here
+// Write your first then apply function her
+export function firstThenApply<T, U>(items: T[], predicate: (item: T) => Boolean, consumer: (item: T) => U): U | undefined{
+  const element: T | undefined = items.find((item) => predicate(item))
+  if(element !== undefined){
+    return consumer(element) 
+  }
+  return undefined
+}
 
 // Write your powers generator here
+export function* powersGenerator(base: bigint): Generator<bigint>{
+  for(let power = 1n; ;power *= base){
+    yield power
+  }
+
+}
 
 // Write your line count function here
+export async function meaningfulLineCount(filename: PathLike): Promise<number>{
+  let num_of_lines = 0
+  try{
+    const data = await open(filename, 'r')
+    for await(const line of data.readLines()){
+      const trimmed_line = line.trim()
+      if(trimmed_line === '' || trimmed_line[0] === '#'){
+        continue
+      } else {
+        num_of_lines++
+      }
+    }    
+  } catch {
+    throw new Error("throws if no such file") 
+  }
+
+  return num_of_lines
+}
 
 // Write your shape type and associated functions here
+interface Sphere{
+  kind: "Sphere"
+  radius: number
+}
+
+interface Box{
+  kind: "Box"
+  width: number
+  length: number
+  depth: number
+}
+
+export type Shape = Sphere | Box
+
+export function surfaceArea(shape: Shape): number{
+  if(shape.kind === "Sphere"){
+    return 4 * Math.PI * shape.radius ** 2
+  } else {
+    return 2 * (shape.width * shape.length + shape.width * shape.depth + shape.length * shape.depth)
+  }
+}
+
+export function volume(shape: Shape): number{
+  if(shape.kind === "Sphere"){
+    return (4/3) * Math.PI * shape.radius ** 3
+  } else {
+    return shape.width * shape.length * shape.depth
+  }
+}
 
 // Write your binary search tree implementation here
+
+export interface BinarySearchTree<T>{
+  size(): number
+  insert(value: T): BinarySearchTree<T>
+  contains(value: T): boolean
+  inorder(): Iterable<T>
+}
+
+export class Empty<T> implements BinarySearchTree<T> {
+  size(): number {
+    return 0
+  }
+
+  insert(value: T): BinarySearchTree<T> {
+    return new Node(value, new Empty, new Empty)
+  }
+
+  contains(value: T): boolean{
+    return false
+  }
+
+  inorder(): Iterable<T> {
+    return []
+  }
+
+  toString(): String{
+    return "()"
+  }
+}
+
+export class Node<T> implements BinarySearchTree<T>{
+  private value: T
+  private left: BinarySearchTree<T>
+  private right: BinarySearchTree<T>
+
+  constructor(value: T, left: BinarySearchTree<T>, right: BinarySearchTree<T>){
+    this.value = value
+    this.left = left
+    this.right = right
+  }
+
+  size(): number{
+    return 1 + this.left.size() + this.right.size()
+  }
+
+  insert(value: T): BinarySearchTree<T>{
+      if (value > this.value) {
+        return new Node(this.value, this.left, this.right.insert(value));
+    } else if (value < this.value) {
+        return new Node(this.value, this.left.insert(value), this.right);
+    } else {
+        return this;
+    }
+
+  }
+
+  contains(value: T){
+    if(value === this.value){
+      return true
+    } else if(value < this.value){
+      return this.left.contains(value)
+    } else {
+      return this.right.contains(value)
+    }
+  }
+
+  *inorder(): Iterable<T> {
+    yield* this.left.inorder()  // Visit left subtree
+    yield this.value;              // Visit current node
+    yield* this.right.inorder()
+  }
+
+  toString(): String{
+    return `(${this.left}${this.value}${this.right})`.replace(/\(\)/g, "");
+
+  }
+}
